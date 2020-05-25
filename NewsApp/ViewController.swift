d3115a61e5c711d3c0b93808068469a53a76d3d3
Ernest D DeFoy III <erniedefoy3@gmail.com>
2020-05-25 10:21:49 -0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ernest DeFoy on 5/25/20.
+//  Copyright © 2020 Ernest DeFoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
