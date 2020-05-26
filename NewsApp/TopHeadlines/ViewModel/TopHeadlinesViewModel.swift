9ea4234922a53760f5cd5cfff81f42b0a3b943c8
Ernest D DeFoy III <erniedefoy3@gmail.com>
2020-05-26 16:06:01 -0400

    redesign - download all images first and then tell tableview to reload. add isLoading variable for shimmer cells

--- NewsApp/TopHeadlines/ViewModel/TopHeadlinesViewModel.swift
+++ NewsApp/TopHeadlines/ViewModel/TopHeadlinesViewModel.swift
@@ -13,17 +13,20 @@ class TopHeadlinesViewModel {
 	let dataManager = NewsAppDataManager()
 	
 	var headlines: NewsApi.TopHeadlines?
-	var items: [Item]?
+	var items: [Item] = []
+	var isLoading = false
 	
 	func loadTopHeadlines(_ completion: @escaping (() -> Void)) {
+		isLoading = true
+		completion()
 		dataManager.newsApiTopHeadlines { [weak self] (status, response) in
 			guard let self = self else { return }
+			self.isLoading = false
 			switch status {
 			case .success:
 				if let model = response {
 					self.headlines = model
-					self.createItems(model)
-					completion()
+					self.createItems(model, completion)
 				}
 			case .error:
 				break
@@ -32,23 +35,57 @@ class TopHeadlinesViewModel {
 	}
 	
 	func numberOfRows(in section: Int) -> Int {
-		return items?.count ?? 0
+		return isLoading ? 10 : items.count
 	}
 	
-	func item(for row: Int) -> Item? {
-		return items?[row]
+	func item(for row: Int) -> Item {
+		return items[row]
+	}
+	
+	func createItems(_ model: NewsApi.TopHeadlines, _ completion: @escaping (() -> Void)) {
+		guard let articles = model.articles else {
+			return
+		}
+		var items: [Item] = []
+		
+		isLoading = true
+
+		let dGroup = DispatchGroup()
+		
+		articles.forEach { article in
+			if let url = article.urlToImage {
+				dGroup.enter()
+				dataManager.downloadImage(from: url) { image in
+					if let image = image {
+						var item = Item(article)
+						item.image = image
+						items.append(item)
+					}
+					dGroup.leave()
+				}
+			}
+		}
+		
+		dGroup.notify(queue: .main) {
+			self.isLoading = false
+			self.items = items
+			completion()
+		}
 	}
 	
 	func createItems(_ model: NewsApi.TopHeadlines) {
-		self.items = model.articles?.map { article -> Item in
+		guard let articles = model.articles else {
+			return
+		}
+		self.items = articles.map { article -> Item in
 			return Item(article)
 		}
 	}
 	
-	func loadImage(_ imageURL: String?, _ index: Int, _ completion: @escaping ((UIImage) -> Void)) {
+	func loadImage(_ imageURL: String?, _ index: Int, _ completion: @escaping ((UIImage?) -> Void)) {
 		guard let url = imageURL else { return }
 		dataManager.downloadImage(from: url, contentMode: .scaleAspectFit) { image in
-			self.items?[index].image = image
+			self.items[index].image = image
 			completion(image)
 		}
 	}
@@ -80,56 +117,6 @@ extension TopHeadlinesViewModel {
 			} else {
 				date = nil
 			}
-		}
-	}
-}
-
-extension Date {
-	
-	func timeSinceString(_ date: Date) -> String? {
-		let interval = self - date
-		
-		let days = Int(interval / 86400)
-		let hours = Int(interval.truncatingRemainder(dividingBy: 86400)) / 3600
-		let minutes = Int(interval.truncatingRemainder(dividingBy: 3600)) / 60
-		let seconds = Int((interval.truncatingRemainder(dividingBy: 3600)).truncatingRemainder(dividingBy: 60))
-		
-		if days > 0 {
-			return "\(days)d ago"
-		} else if hours > 0 {
-			return "\(hours)h ago"
-		} else if minutes > 0 {
-			return "\(minutes)m ago"
-		} else if seconds > 0 {
-			return "\(seconds)s ago"
-		} else {
-			return nil
-		}
-	}
-
-    static func - (lhs: Date, rhs: Date) -> TimeInterval {
-        return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
-    }
-
-}
-
-extension String {
-	func toDate() -> Date? {
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-		guard let currentDate = dateFormatter.date(from: self) else {
-			return nil
-		}
-		return currentDate
-	}
-	
-	func formattedDateString() -> String? {
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "MMM dd, yyyy"
-		if let date = self.toDate() {
-			return dateFormatter.string(from: date)
-		} else {
-			return nil
 		}
 	}
 }
