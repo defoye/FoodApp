ac579db4f28476c406131c102d424996abc41a14
Ernest D DeFoy III <erniedefoy3@gmail.com>
2020-05-30 02:34:55 -0400

    Add comments

--- NewsApp/Base/BaseViewController.swift
+++ NewsApp/Base/BaseViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class BaseViewController: UIViewController {
 	
+	/// Previous y coordinate of scrolling position
 	private var previousOffset: CGFloat = 0
+	/// Whether or not data has been fetched due to the scrolling threshold
 	var nextPageCalled: Bool = false
-	var inScrollFetchRange: Bool = false
+	/// Whether or not current scroll position is range for fetching more data.
+	private(set) var inScrollFetchRange: Bool = false
+	/// Number of scrollView frame lengths. Used to compute scrolling threshold.
 	var fetchDistanceMultiplier: CGFloat {
 		return 1
 	}
