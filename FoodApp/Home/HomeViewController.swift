4df5506d00ceff240a93140b152ff18029c9aef2
Ernest DeFoy <erniedefoy3@gmail.com>
2020-11-02 16:53:38 -0500

    MOBL- implement home view controller with 3 top recipes

--- FoodApp/Home/HomeViewController.swift
+++ FoodApp/Home/HomeViewController.swift
@@ -10,15 +10,75 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
+    enum Item: Hashable {
+        case recipeSimilar(_ model: FirebaseAPI.TopRecipesSearchResults.ResponseModel)
+    }
+    
+    lazy var dataSource: UITableViewDiffableDataSource<Section, Item> = {
+        UITableViewDiffableDataSource<Section, Item>(tableView: tableView) { [weak self] (tableView, indexPath, item) -> UITableViewCell? in
+            switch item {
+            case .recipeSimilar(let model):
+                return tableView.configuredCell(RecipePreviewTableViewCell.self, identifier: RecipePreviewTableViewCell.reuseIdentifier) { cell in
+                    cell.configure(model)
+                }
+            }
+        }
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(RecipePreviewTableViewCell.self, forCellReuseIdentifier: RecipePreviewTableViewCell.reuseIdentifier)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = dataSource
         view.backgroundColor = .blue
-        
+            
+        view.addSubview(tableView)
+        tableView.pin(to: view.safeAreaLayoutGuide)
+
         self.title = "Home"
         
-        FirebaseDataManager.shared.fetchTopRecipes(numberOfResults: 20) { models in
-            
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        
+        snapshot.appendSections([.main])
+        
+        let fetchAmount = Constants.Ints.homeTopRecipesCount.rawValue
+        FirebaseDataManager.shared.fetchTopRecipes(numberOfResults: fetchAmount) { models in
+            let items = models.map { model -> Item in
+                return .recipeSimilar(model)
+            }
+            snapshot.appendItems(items)
+            self.dataSource.apply(snapshot)
+            items.forEach { item in
+                switch item {
+                case .recipeSimilar(let model):
+                    RecipeDataManager.shared.downloadImage(from: model.imageURL) { image in
+                        if let image = image {
+                            let ratio = image.size.width / image.size.height
+                            if abs(ratio - 1) <= 0.5 {
+                                model.image = image
+                                snapshot.reloadItems([item])
+                                self.dataSource.apply(snapshot)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
