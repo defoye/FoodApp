2cfd23cbfcda44887db60caa855334ce7cbfac23
Ernest DeFoy <erniedefoy3@gmail.com>
2020-11-11 16:49:03 -0500

    MOBL- add utility extension for hiding keyboard when background is tapped

--- FoodApp/Utility/Extensions/Extensions.swift
+++ FoodApp/Utility/Extensions/Extensions.swift
@@ -82,3 +82,15 @@ extension UIImageView {
         self.clipsToBounds = true
     }
 }
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
